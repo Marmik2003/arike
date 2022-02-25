7fa63f5e8ca63630b9353e936ff0270a5b138922
Marmik Patel <marmikpatel250@gmail.com>
2022-02-25 21:41:36 +0530

    Filter JS updates

--- arike/static/js/health_center/patients/list.js
+++ arike/static/js/health_center/patients/list.js
@@ -1,14 +1,8 @@
 $("#filter_form").submit(function(e) {
     e.preventDefault();
-    const ward = $("#input-ward").val();
-    const kind = $("#input-kind").val();
+    const facility = $("#input-facility").val();
     let params = parseURLParams();
     params.page = 1;
-    if (ward.trim() !== '') {
-        params.ward = ward;
-    } else {
-        delete params.ward;
-    }
-    params.kind = kind;
+    params.facility = facility;
     window.location.href = window.location.href.split('?')[0] + '?' + joinParam(params);
 });
